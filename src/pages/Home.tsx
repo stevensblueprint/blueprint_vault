b2d40d61d28f01d678a059bec36df973124339b5
miguel-merlin <mmerlin@stevens.edu>
2026-01-31 04:43:56 -0500

    feat: adde api call in pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useApi } from "../api/useApi";
 import { useAuth } from "../contexts/AuthContext";
+import type {
+  ListPasswordEntriesResponse,
+  PasswordEntry,
+} from "../types/passwords";
 
 export const Home: React.FC = () => {
-  const { user, loading } = useAuth();
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const { api } = useApi();
+  const { user } = useAuth();
+  const [error, setError] = useState<string | null>(null);
+  const [passwords, setPasswords] = useState<PasswordEntry[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPasswords = async () => {
+      try {
+        const data = await api.get<ListPasswordEntriesResponse>("/passwords");
+        setPasswords(data.items);
+      } catch (error) {
+        console.error("Failed to fetch passwords:", error);
+        setError("Failed to load passwords.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    setLoading(true);
+    fetchPasswords();
+  }, [api]);
+
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome, {user?.firstName}!</p>
+      {loading && <p>Loading passwords...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {passwords && (
+        <ul>
+          {passwords.map((entry: PasswordEntry) => (
+            <li key={entry.entryId}>
+              {entry.label} - {entry.username}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
